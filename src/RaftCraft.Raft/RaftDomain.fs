706609211a4de7eb919e8070229db153996c965b
chrisp9 <chris_powell9@outlook.com>
2018-11-18 22:33:43 +0000

    Add comment documenting state transition

--- src/RaftCraft.Raft/RaftDomain.fs
+++ src/RaftCraft.Raft/RaftDomain.fs
@@ -6,4 +6,8 @@ open RaftStateMachine
 
 type DomainEvent =
     | Request of RequestMessage
+
+    // Transition(oldState -> newState) We care about old state, since some state transitions will be considered invalid,
+    // as they are placed on queues. Sometimes we may want to disregard a transition of state has changed whilst on the 
+    // RaftNode agent queue.
     | Transition of RaftState * RaftState
