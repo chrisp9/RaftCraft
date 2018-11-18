e0740b276551d7315b2d4d0b2f514733df147ca5
chrisp9 <chris_powell9@outlook.com>
2018-11-18 19:29:29 +0000

    Add comment in operators.fs

--- src/RaftCraft.Raft/Operators.fs
+++ src/RaftCraft.Raft/Operators.fs
@@ -1,5 +1,7 @@
 ﻿module RaftCraft.Operators
 
+// Wraps a reference type as Option<T>. Needed for interop with classes passed in from C# since
+// in C# things can be null. We want those to be Option.None in F# so that we can pattern match nicely.
 let inline (!?) value =
     match value with
         | null -> None
